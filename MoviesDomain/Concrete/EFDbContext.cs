cc8e62c09984c056874f0e396c2aaf0e9a7003c7
Jonathan Hayes <jonathan.hayes32@outlook.com>
2018-11-20 10:46:59 -0500

    git commit (add) to command line

---
+++ MoviesDomain/Concrete/EFDbContext.cs
@@ -0,0 +1,10 @@
+﻿using Movies.Domain.Entities;
+using System.Data.Entity;
+
+namespace Movies.Domain.Concrete
+{
+   public class EFDbContext : System.Data.Entity.DbContext
+    {
+       public DbSet<Movie> Movies { get; set; }
+    }
+}
